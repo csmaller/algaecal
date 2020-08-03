a595edad87df9f27547e26cb430833fd20733105
corey smaller <coreysmaller@gmail.com>
2020-08-03 16:38:21 -0600

    modified wistiaPlayer component just to show custom video and not inject the video ID

--- components/video/WistiaPlayer.vue
+++ components/video/WistiaPlayer.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <script v-bind:src="`https://fast.wistia.com/embed/medias/${video_id}.jsonp`" async></script>
-    <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+    <script src='https://fast.wistia.com/embed/medias/w35569fyay.jsonp' async></script>
+    <script src='https://fast.wistia.com/assets/external/E-v1.js' async></script>
     <div
-      v-bind:class="`wistia_embed wistia_async_${video_id}`"
-      style="height:360px;position:relative;width:640px"
+      class='wistia_embed wistia_async_w35569fyay'
+      style='height:360px;position:relative;width:640px'
     >
       <div
-        class="wistia_swatch"
-        style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"
+        class='wistia_swatch'
+        style='height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;'
       >
         <img
-          v-bind:src="`https://fast.wistia.com/embed/medias/${video_id}/swatch`"
-          style="filter:blur(5px);height:100%;object-fit:contain;width:100%;"
+          src='https://fast.wistia.com/embed/medias/w35569fyay/swatch'
+          style='filter:blur(5px);height:100%;object-fit:contain;width:100%;'
           alt
-          aria-hidden="true"
-          onload="this.parentNode.style.opacity=1;"
+          aria-hidden='true'
+          onload='this.parentNode.style.opacity=1;'
         />
       </div>
     </div>
@@ -24,6 +24,6 @@
 
 <script>
 export default {
-  props: ["video_id"],
+  props: ['video_id'],
 };
 </script>
